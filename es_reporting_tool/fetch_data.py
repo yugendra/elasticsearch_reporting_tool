f47b4c36236df789706b594243ba4c4f48e7994a
yugendra <yugendra.mail@gmail.com>
2017-12-31 17:30:08 +0530

    write json data into file

--- es_reporting_tool/fetch_data.py
+++ es_reporting_tool/fetch_data.py
@@ -1,18 +1,41 @@
 from elasticsearch import Elasticsearch
 from elasticsearch_dsl import Search
 import json
+import os.path
 
-def fetch_data()
+def file_cleanup():
     '''
-        Fetch the data from elasticsearch.
-        Apply filter on data before fetching.
-        Returns only specified fields
-        Convert fetched objects into json format
+    Remove old files and
+    '''
+    try:
+        os.remove('data.json')
+    except OSError:
+        pass
+        
+    try:
+        os.remove('data.csv')
+    except OSError:
+        pass
+
+def fetch_data():
+    '''
+    Fetch the data from elasticsearch.
+    Apply filter on data before fetching.
+    Returns only specified fields
+    Convert fetched objects into json format and write into the file.
     '''
     client = Elasticsearch([{'host': 'localhost', 'port': 9200}])
-    s = Search(using=client, index="filebeat*")
-    s = s.query("match", sender="athagroup.in")
+    s = Search(using=client, index='filebeat*')
+    s = s.query('match', sender='athagroup.in')
     s = s.source(['sender','subject'])
+    
+    fh = open('data.json', 'w')
      
     for hit in s.scan():
-        print json.dumps(hit, default=lambda o: o.__dict__)
+        fh.write(json.dumps(hit, default=lambda o: o.__dict__))
+
+    fh.close()
+        
+if __name__ == "__main__":
+    file_cleanup()
+    #fetch_data()
